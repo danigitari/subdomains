99682b9b0b4ef369b817ff06572cc8fa3475963c
danigitari <53410851+danigitari@users.noreply.github.com>
2020-12-27 19:08:56 +0300

    Add files via upload

---
+++ subdomain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "enter domain name: " domain
+if [ ! -d "$domain"  ]; then
+        mkdir -p $domain
+fi
+assetfinder --subs-only $domain > $domain/output.txt
+amass enum -silent -passive -norecursive -noalts -d $domain >>$domain/output_subs.txt
+sublist3r -d $domain -o$domain/output_subs.txt
+findomain -q -t $domain >> $domain/output_subs.txt
+subfinder -silent -d $domain -o $domain/output_subs.txt
+~/subbrute/./subbrute.py $domain >> $domain/output_subs.txt
+cat $domain/output.txt  | sort -u | wc  -l
