a32300782df64470dcd4d0b23fe52517c7bf2630
danigitari <53410851+danigitari@users.noreply.github.com>
2022-06-03 11:14:52 +0300

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-read -p "enter domain name: " domain
-if [ ! -d "$domain"  ]; then
-        mkdir -p $domain
-fi
-assetfinder --subs-only $domain > $domain/output_subs.txt
-amass enum -silent -passive -norecursive -noalts -d $domain >>$domain/output_subs.txt
-sublist3r -d $domain -o$domain/output_subs.txt
-./findomain-linux -q -t $domain >> $domain/output_subs.txt
-subfinder -silent -d $domain -o $domain/output_subs.txt
-~/subbrute/./subbrute.py $domain >> $domain/output_subs.txt
-cat $domain/output.txt  | sort -u | wc  -l
+mkdir $1;
+cd $1;
+echo -e "\e[1;34m gathering subdomains with findomain ....\e[0m"
+findomain-linux -q -t $1 | sort -u > subdomains_$1.txt ;
+echo -e "\e[1;34m gathering subdomains with subfinder ....\e[0m"
+subfinder -silent -d $1 | sort -u > subdomains_$1.txt ;
+echo -e "\e[1;34m gathering subdomains with amass ....\e[0m"
+amass enum -d $1 | sort -u > subdomains_$1.txt ;
+echo -e "\e[1;34m gathering subdomains with assetfinder ....\e[0m"
+assetfinder --subs-only $1 | sort -u > subdomains_$1.txt;
+echo -e "\e[1;34m gathering subdomains with crt.sh ....\e[0m"
+curl https://crt.sh/?q=%.$1 | grep "$1" | cut -d '>' -f2 | cut -d '<' -f1 | grep -v " " | sort -u > subdomains_$1.txt
+echo -e "\e[1;34m Probing subdomains with httprobe ....\e[0m"
+cat subdomains_$1.txt | sort -u | httprobe -c 50 > hosts_$1.txt;
